de686edb7a9d4a43f54673984dcc635fd4f7e20a
Dhruv Jain <jaindhru005@gmail.com>
2026-01-18 11:53:53 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:3000'],
+    origin: ['http://localhost:5173','http://localhost:3000','https://thumblify-alpha-seven.vercel.app'],
     credentials: true
 }));
 
